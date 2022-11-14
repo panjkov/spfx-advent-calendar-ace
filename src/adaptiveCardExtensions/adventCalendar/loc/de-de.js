ed3355708a10e6809f84890d7f2d1a81a1f9d204
Dragan Panjkov <panjkov@hotmail.com>
2022-11-15 00:45:02 +0100

    Finalized functionality, documentation

---
+++ src/adaptiveCardExtensions/adventCalendar/loc/de-de.js
@@ -0,0 +1,15 @@
+define([], function() {
+  return {
+    "PropertyPaneDescription": "Die Karte lädt Daten aus einer mit 24 Links gefüllten SharePoint-Liste und kann vom 1.12. bis zum 24.12. während der sogenannten „Adventszeit“ aktiviert werden. Jeden Tag wird dem Benutzer eine andere Karte präsentiert, die mit einer anderen Seite im Intranet oder Internet verlinkt ist. Wenn der Benutzer auf die Karte klickt, wird die verknüpfte Webseite im Browser geöffnet.",
+    "TitleFieldLabel": "Card Titel",
+    "Title": "Adaptive Card Extension",
+    "SubTitle": "Quick view",
+    "PrimaryText": "SPFx Adaptive Card Extension",
+    "Description": "Create your SPFx Adaptive Card Extension solution!",
+    "QuickViewButton": "Schnellansicht",
+    "DoorPrefixLabel": "Türchenprefix text",
+    "OpenHereTextLabel": "Öffnen hier text",
+    "ListTitleLabel": "List Name",
+    "VisitDoorButton": "Besuchen"
+  }
+});
